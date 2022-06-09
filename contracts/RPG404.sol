74466f93b93f1644eb7a540e22c2ce368ff3ed27
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-10 04:28:34 +0800

    添加RPG404的合约和测试用例文件

--- contracts/RPG404.sol
+++ contracts/RPG404.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract NFT is ERC721Enumerable, Ownable {
+contract RPG404 is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
     string baseURI;
---
+++ migrations/3_rpg_404_migration.js
@@ -0,0 +1,5 @@
+const RPG404 = artifacts.require('RPG404');
+
+module.exports = function (deployer) {
+  deployer.deploy(RPG404);
+};
